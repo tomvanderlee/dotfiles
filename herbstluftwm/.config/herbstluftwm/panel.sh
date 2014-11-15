f691f01606af95670e0983884062a1d70ec7bce9
Tom van der Lee <t0m.vd.l33@gmail.com>
2014-11-15 23:29:05 +0100

    Moved some panel code to seperate file

--- herbstluftwm/.config/herbstluftwm/panel.sh
+++ herbstluftwm/.config/herbstluftwm/panel.sh
@@ -5,7 +5,8 @@ hc() {
 }
 
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source "$dir/theme"
+source "$dir/theme.sh"
+source "$dir/panel_indicators.sh"
 
 monitor=${1:-0}
 
@@ -41,97 +42,12 @@ fi
     #   <eventname>\t<data> [...]
     # e.g.
     #   date    ^fg(#efefef)18:33^fg(#909090), 2013-10-^fg(#efefef)29
-
     while true ; do
-		# Music
-		player_status=$(playerctl status)
-		if [ $player_status = "Playing" ]; then
-			player_artist=$(playerctl metadata artist)
-			player_title=$(playerctl metadata title)
-			playing="$player_title - $player_artist"
-
-			if [ "$current" != "$playing" ] ; then
-				current=$playing
-				scrolling=$current
-			elif [ ${#scrolling} -gt "24" ] ; then
-				scrolling=${scrolling:1}
-			else
-				scrolling=$current
-			fi
-
-			echo -e "music\t\ue05c ${scrolling:0:24}"
-		else
-			echo -e "music\toff"
-		fi
-
-		# Volume
-		if pgrep pulseaudio >> /dev/null ; then
-			volumes=$(\
-				amixer get Master | \
-				grep "Front Right: Playback"\
-			)
-			vol=$(\
-				echo $volumes | \
-				sed "s/.*\[\([0-9]*\)%\].*/\1/"\
-			)
-			if [ -z $vol ] ; then
-				echo -e "volume\toff"
-			else
-				echo -e "volume\t%{F$acolor_fg}\ue05d $vol%%%{F-}"
-			fi
-		else
-			echo -e "volume\toff"
-		fi
-
-		# Network
-		read lo int1 int2 <<< `ip link | sed -n 's/^[0-9]: \(.*\):.*$/\1/p'`
-		if iwconfig $int1 >/dev/null 2>&1; then
-			wifi=$int1
-			eth=$int2
-		else
-			wifi=$int2
-			eth=$int1
-		fi
-
-		ip link show $eth | grep 'state UP' >/dev/null && int=$eth || int=$wifi
-
-		if [ $int == "wlp3s0" ] ; then
-			iwconfig=$(iwconfig $int)
-			ssid=$(\
-				echo $iwconfig | \
-				sed "s/.*ESSID:\(\".*\"\).*/\1/" | \
-				sed "s/.*\(off\/any\).*/\"\1\"/" | \
-				sed "s/.*\"\(.*\)\".*/\1/"\
-			)
-
-			if [ $ssid != "off/any" ] ; then
-				echo -e "net\t\ue0f3 $ssid"
-			else
-				echo -e "net\toff"
-			fi
-
-		elif [ $int == "enp2s0" ] ; then
-			echo -e "net\t\ue0af ethernet"
-		else
-			echo -e "net\toff"
-		fi
-
-		# Battery
-		if $(test -e /sys/class/power_supply/BAT1) ; then
-			bat_lvl=$(cat /sys/class/power_supply/BAT1/capacity)
-			if [ $bat_lvl -lt 15 ] ;	then
-				bat_color=$acolor_accent
-			else
-				bat_color=$acolor_fg
-			fi
-			state=$(cat /sys/class/power_supply/BAT1/status)
-			echo -e "battery\t%{F$acolor_fg}\ue03b %{F$bat_color}$bat_lvl%{F$acolor_fg}%%%{F-}"
-		else
-			echo -e "battery\toff"
-		fi
-
-		# Time
-        echo -e $(date +$"date\t%{F$acolor_fg}%H:%M:%S %{F$acolor_fg}(%d-%m-%Y)%{F-}")
+		music
+		volume
+		network
+		battery
+		clock
         sleep 1 || break
     done > >(uniq_linebuffered) &
     childpid=$!
