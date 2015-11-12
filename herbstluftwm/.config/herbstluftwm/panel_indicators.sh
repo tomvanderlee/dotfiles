52a40f15945c1a2d0283aba7be40945c6f472749
Tom van der Lee <t0m.vd.l33@gmail.com>
2015-11-12 20:39:25 +0100

    Added mpc support to panel

--- herbstluftwm/.config/herbstluftwm/panel_indicators.sh
+++ herbstluftwm/.config/herbstluftwm/panel_indicators.sh
@@ -12,17 +12,22 @@ music()
     # Music
     mpd_status=$(mpc | sed -nr "s/^\[(.*)\].*$/\1/p")
     if [ $mpd_status = "playing" ]; then
-        player_title=$(mpc -f "%title%" current)
         player_artist=$(mpc -f "%artist%" current)
+        player_title=$(mpc -f "%title%" current)
         if [ -n "$player_artist" ]; then
-            playing="$player_title - $player_artist"
+            playing="$player_artist - $player_title"
         else
             playing="$player_title"
         fi
     elif [ $(playerctl status) = "Playing" ]; then
         player_artist=$(playerctl metadata artist)
         player_title=$(playerctl metadata title)
-        playing="$player_title - $player_artist"
+        if [ -n "$player_artist" ] && [ -n "$player_title" ]; then
+            playing="$player_artist - $player_title"
+        else
+            now_playing=$(playerctl metadata vlc:nowplaying)
+            playing="$now_playing"
+        fi
     else
         playing=""
     fi
