31e52e45c77ed4965b46723f514085afd280f66c
Tom van der Lee <t0m.vd.l33@gmail.com>
2015-11-30 21:02:35 +0100

    Replaced dmenu by rofi

---
+++ herbstluftwm/.config/herbstluftwm/rofi_theme.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+theme_dir="$HLWM_CONF_DIR/themes"
+current=$(readlink "$theme_dir/current")
+themes=$(ls "$theme_dir" | grep -vE "current|template|README\.md|$current")
+theme=$(echo "$themes" | rofi -dmenu -p "theme:" $@)
+
+if [ -n "$theme" ] && [ -f "$theme_dir/$theme" ]; then
+	ln -sf "$theme" "$theme_dir/current" && herbstclient reload
+fi
+
