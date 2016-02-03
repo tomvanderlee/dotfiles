d122825978da0e274d466b066b2407856d378a5b
Tom van der Lee <t0m.vd.l33@gmail.com>
2016-02-03 10:21:53 +0100

    Updates to various files

--- herbstluftwm/.config/herbstluftwm/rofi_theme.sh
+++ herbstluftwm/.config/herbstluftwm/rofi_theme.sh
@@ -2,7 +2,7 @@
 
 theme_dir="$HLWM_CONF_DIR/themes"
 current=$(readlink "$theme_dir/current")
-themes=$(ls "$theme_dir" | grep -vE "current|template|README\.md|$current")
+themes=$(ls "$theme_dir" | grep -vE "^current|template|README\.md|$current\$")
 theme=$(echo "$themes" | rofi -dmenu -p "theme:" $@)
 
 if [ -n "$theme" ] && [ -f "$theme_dir/$theme" ]; then
